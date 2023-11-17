8d67a1acb00a9fc70c659d36a7db5c480955d344
LaughingLeader <laughingleader@gmail.com>
2023-11-17 16:43:13 -0600

    Clean usings

--- src/Core/Models/NexusMods/NexusModsObservableApiLimits.cs
+++ src/Core/Models/NexusMods/NexusModsObservableApiLimits.cs
@@ -1,13 +1,9 @@
-﻿using NexusModsNET.DataModels;
+﻿using NexusModsNET;
 
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DivinityModManager.Models.NexusMods
 {
