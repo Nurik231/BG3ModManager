cb22ca474dcfbdcc63c4c56d6381a9686576ea34
LaughingLeader <laughingleader@gmail.com>
2023-12-10 13:53:09 -0600

    Fix binding errors

--- src/GUI/Controls/DownloadActivityBar.xaml.cs
+++ src/GUI/Controls/DownloadActivityBar.xaml.cs
@@ -2,21 +2,6 @@
 
 using ReactiveUI;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace DivinityModManager.Controls
 {
 	public class DownloadActivityBarBase : ReactiveUserControl<DownloadActivityBarViewModel> { }
@@ -29,6 +14,7 @@ namespace DivinityModManager.Controls
 
 			this.WhenActivated(d =>
 			{
+				DataContext = ViewModel;
 				if(ViewModel != null)
 				{
 					this.OneWayBind(ViewModel, vm => vm.CurrentValue, view => view.TaskProgressBar.Value);
